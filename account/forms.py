ab840fca4c2028eaddb6032c31ed30008d2801ae
Jaemyun Lee <acetrack@gmail.com>
2017-11-30 16:38:23 +0900

    inputtype을 password로 변경하였음

--- account/forms.py
+++ account/forms.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
+        password = forms.CharField(widget=forms.PasswordInput)
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
