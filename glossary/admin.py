c25ee5b6d38d1b04ca8d527da87512c5861bd194
Jaemyun Lee <acetrack@gmail.com>
2017-12-14 21:29:44 +0900

    - comment 삭제

--- glossary/admin.py
+++ glossary/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Glossary
 
 
-# 이곳에 코드를 넣으면 admin에서 table을 볼 때 여기서 정리한 대로 보여준다
 class MyGlosarry(admin.ModelAdmin):
     list_display = ('word', 'frequency', 'account', 'glossary_title')
 
