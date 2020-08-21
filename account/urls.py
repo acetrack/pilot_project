8456705590abe21c2d35d76d69b03b7a44b08a26
Lee Jaemyun <acetrack@gmail.com>
2020-08-22 00:35:59 +0900

    필요없는 account folder 삭제 => allauth를 사용하면서 필요가 없어졌다. 원래 CSS를 찾아줌. 홈페이지에 이미지 추가 카카오 로그인을 넣었지만, 로그인은 되지 않는다.

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from . import views
-
-app_name = 'account'
-urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('sign_up/', views.sign_up, name='sign_up'),
-]
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # selecting google and naver
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.kakao',
 
     'home',
     # 'account',
@@ -77,7 +78,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
@@ -141,6 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 LOGIN_REDIRECT_URL = '/glossary/to_remember_list'
 LOGOUT_REDIRECT_URL = '/'
