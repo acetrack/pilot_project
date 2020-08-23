fee19d6edd445886e5c9d2fa15ca44dc7d6cd9fb
Lee Jaemyun <acetrack@gmail.com>
2020-08-23 10:51:28 +0900

    STATIC_ROOT를 설정함. 배포를 할 경우, static file들을 한 곳에 모아야 함. 로컬에서는 runserver가 알아서 처리해 주는 것이 배포를 할 경우, 처리하지 못하는 이유. ( 왜??? )

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 LOGIN_REDIRECT_URL = '/glossary/to_remember_list'
 LOGOUT_REDIRECT_URL = '/'
